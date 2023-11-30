e0a6f9e544d4934c92377b26ecde7e2eedad2c81
Sainul Abid <sainulabidofficial@gmail.com>
2023-11-30 23:56:13 +0530

    fix: checking cookie in header

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -2,16 +2,20 @@ import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
   try {
-    console.log(req.headers)
-    console.log(req.headers.cookie.split('=')[1])
-    let verified = jwt.verify(
-      req.headers.cookie.split('=')[1],
+    if (req.headers?.cookie) {
+       let verified = jwt.verify(
+      req.headers?.cookie?.split('=')[1],
       process.env.JWT_SECRET
     )
     if (verified) {
       req.userId = verified.userId
       next()
     }
+    }
+    else{
+      res.status(403).send('login first')
+    }
+   
   } catch (e) {
     res.status(401).send(e.message)
   }
