1bf05349efe81098c05c3cc918172afb600089fc
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-01 23:23:59 +0530

    feat:get user posts and details in params

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import Post from '../models/postModel.js'
+import User from '../models/userModel.js'
 export const user = (req, res) => {
   console.log(req.userId)
   res.json('logged a valid user')
@@ -15,5 +16,24 @@ export const createPost = async (req, res) => {
     res.status(201).json(newPost)
   } catch (e) {
     res.status(400).send(e.message)
+  }
+}
+export const getUserProfile = async (req, res) => {
+  try {
+    if (req.params?.username) {
+      let userId = await User.exists({ username: req.params.username })
+      if (userId) {
+        let userPosts = await Post.find({ user: userId })
+          .populate({
+            path: 'user',
+            select: '-password',
+          })
+          .exec()
+        return res.json(userPosts)
+      }
+      return res.status(404).json({ message: 'no user found' })
+    }
+  } catch (e) {
+    res.status(500).send(e.message)
   }
 }
--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,10 +1,11 @@
 import expres from 'express'
-import { createPost } from '../../controllers/userControllers.js'
+import { createPost , getUserProfile } from '../../controllers/userControllers.js'
 import { getPosts } from '../../controllers/feedControllers.js'
 
 const router = expres.Router()
 
 router.route('/').get(getPosts)
 router.route('/post').post(createPost)
+router.route('/:username').get(getUserProfile)
 
 export default router
