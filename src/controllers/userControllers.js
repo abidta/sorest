a2c92a57534968c69e2cc7912ae9bb0cbb062eab
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-02 02:38:39 +0530

    patch:push post id to user posts
    for cleaner array for client

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -6,12 +6,15 @@ export const user = (req, res) => {
   res.json('logged a valid user')
 }
 export const createPost = async (req, res) => {
-  console.log(req.body)
   try {
     let newPost = await Post.create({
       user: new mongoose.Types.ObjectId(req.userId),
       content: req.body.content,
     })
+    let isUpdatedUserPosts = await User.updateOne(
+      { _id: newPost.user },
+      { $push: { posts: newPost._id } }
+    )
     console.log(newPost)
     res.status(201).json(newPost)
   } catch (e) {
@@ -23,10 +26,10 @@ export const getUserProfile = async (req, res) => {
     if (req.params?.username) {
       let userId = await User.exists({ username: req.params.username })
       if (userId) {
-        let userPosts = await Post.find({ user: userId })
+        let userPosts = await User.find({ _id: userId }, '-password')
           .populate({
-            path: 'user',
-            select: '-password',
+            path: 'posts',
+            select: '-user',
           })
           .exec()
         return res.json(userPosts)
