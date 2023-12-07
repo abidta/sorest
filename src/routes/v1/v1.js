9192c5d9968b821fdb25ff280d59a635f755d103
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-08 00:47:52 +0530

    fix:corrected api endpoints with swagger autogen

--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -3,8 +3,6 @@ import authRoutes from './authRoutes.js'
 import userRoutes from './userRoutes.js'
 import { verifyToken } from '../../middlewares/verifyToken.js'
 
-const router = express
-  .Router()
-  .use('/auth', authRoutes)
-  .use('/',verifyToken, userRoutes)
+const router = express.Router()
+router.use('/auth', authRoutes).use('/', verifyToken, userRoutes)
 export default router
--- swagger.js
+++ swagger.js
@@ -2,14 +2,14 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
+    title: 'soREST API',
     description: 'Description'
   },
   host: 'localhost:3000'
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./src/routes/v1/v1.js','./src/routes/v1/authRoutes.js','./src/routes/v1/userRoutes.js'];
+const routes = ['./src/server.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
