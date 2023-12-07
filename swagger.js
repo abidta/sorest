d8e84f81ea567e06361cf3621f3accb1e114d491
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-07 14:15:13 +0530

    chore:swagger updated

--- swagger.js
+++ swagger.js
@@ -8,8 +8,8 @@ const doc = {
   host: 'localhost:3000'
 };
 
-const outputFile = '../swagger-output.json';
-const routes = ['./routes/v1/v1.js','./routes/v1/authRoutes.js','./routes/v1/userRoutes.js'];
+const outputFile = './swagger-output.json';
+const routes = ['./src/routes/v1/v1.js','./src/routes/v1/authRoutes.js','./src/routes/v1/userRoutes.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
