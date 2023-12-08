388c859f13dd6eb5736f866d40e831804eb1f095
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-09 02:33:15 +0530

    feat: likes and dislikes for posts

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
-import mongoose from 'mongoose'
+import mongoose, { isValidObjectId } from 'mongoose'
 import Post from '../models/postModel.js'
 import User from '../models/userModel.js'
+
 export const user = (req, res) => {
   console.log(req.userId)
   res.json('logged a valid user')
@@ -64,4 +65,45 @@ export const updatePassword = async (req, res) => {
   } catch (e) {
     res.status(400).json(e.message)
   }
+}
+export const likePost = async (req, res) => {
+  const postId = req.params.id
+  // 'req.query' may have multiple queries. currently only take first query.
+  let { action } = req.query
+  action = Array.isArray(action) ? action[0] : action
+  console.log(req.query)
+  console.log(action)
+  try {
+    if (!isValidObjectId(postId)) {
+      throw new Error(`'${postId}' is not valid objectId`)
+    }
+    let post = await Post.findById({ _id: postId })
+    console.log(post, 'post')
+    if (!post) {
+      throw new Error('post not found')
+    }
+    let userId = req.userId
+    switch (action) {
+      case 'like':
+        if (post.likes.indexOf(req.userId) == -1) {
+          post.likes.push(req.userId)
+          await post.save()
+        }
+        break
+      case 'unlike':
+        if (post.likes.indexOf(req.userId) !== -1) {
+          post.likes.pull(req.userId)
+          await post.save()
+        }
+        break
+      default:
+        res.status(400).send('query is not valid')
+        break
+    }
+    res.status(200).send('done')
+  } catch (e) {
+    res.status(404).send(e.message)
+  }
+
+  console.log('hmm')
 }
--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,11 +1,12 @@
 import expres from 'express'
-import { createPost , getUserProfile, updatePassword, updateUserDeatails } from '../../controllers/userControllers.js'
+import { createPost , getUserProfile, updatePassword, updateUserDeatails, likePost } from '../../controllers/userControllers.js'
 import { getPosts } from '../../controllers/feedControllers.js'
 
 const router = expres.Router()
 
 router.route('/').get(getPosts)
 router.route('/post').post(createPost)
+router.route('/post/:id').put(likePost)
 router.route('/account/update').put(updateUserDeatails)
 router.route('/account/update/password').put(updatePassword)
 router.route('/:username').get(getUserProfile)
