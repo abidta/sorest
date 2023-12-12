d56390782089c7196b3142f62ccafd0262d85dc4
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-13 01:24:12 +0530

    patch: multiple arguments supported in check objectId

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,9 +1,21 @@
 import createError from 'http-errors'
 import { isValidObjectId } from 'mongoose'
 
+function err(id) {
+  return createError(400, `'${id}' is invalid ObjectId`)
+}
+//multiple arguments, you must pass in a array
 export const checkObjectId = (id) => {
+  if (Array.isArray(id)) {
+    id.forEach((objectId) => {
+      if (!isValidObjectId(objectId)) {
+        throw err(objectId)
+      }
+    })
+    return true
+  }
   if (!isValidObjectId(id)) {
-    throw createError(400, `'${id}' is invalid ObjectId`)
+    throw err(id)
   }
   return true
 }
